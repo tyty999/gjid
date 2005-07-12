a63e604fcc9761d88fab514f0e978d3c58e6814a
msharov <msharov>
2005-07-12 19:36:15 +0000

    Reformatted Streamable to compile on 4.0.0

--- include/streamable.h
+++ include/streamable.h
@@ -1,62 +1,42 @@
-/* streamable.h
-**
-**	Defines a base class for classes that use iostream.
-** IO to file and std[in,out]. Overloaded << and >> for the whole family.
-**
-** Warning: do not include <unistd,h>, <stdio.h> or any other C output headers.
-**		They contain conflicts with <iostream.h>
-**
-** includes:
-**	iostream.h	- for <<, >> and streams
-**	fstream.h	- for ifstream and ofstream
-*/
+// streamable.h
+//
 
-#ifndef __STREAMABLE_H
-#define __STREAMABLE_H		 
+#ifndef STREAMABLE_H_46485D7840AF7A70129D1687392DDAC5
+#define STREAMABLE_H_46485D7840AF7A70129D1687392DDAC5
 
 #include <iostream>
 #include <fstream>
 using namespace std;
 
+/// \brief Defines a base class for classes that use iostream.
+/// IO to file and std[in,out]. Overloaded << and >> for the whole family.
+///
 class Streamable {
 public:
-    inline void			Load (char * filename);
-    inline void			Save (char * filename);
-
-    virtual inline void 	Read (istream&) {};
-    virtual inline void 	Write (ostream&) {};
-
-    virtual inline void 	Read (ifstream&) {};
-    virtual inline void 	Write (ofstream&) {};
+    inline virtual     ~Streamable (void) {}
+    inline void		Load (const char * filename)	{ ifstream is (filename); Read (is); }
+    inline void		Save (const char * filename)	{ ofstream os (filename); Write (os); }
+    inline virtual void Read (istream&)		{}
+    inline virtual void Write (ostream&)	{}
+    inline virtual void	Read (ifstream&)	{}
+    inline virtual void	Write (ofstream&)	{}
 };
-		   
-inline void Streamable :: Load (char * filename)
-{
-ifstream is (filename);
-    Read (is);
-}
 
-inline void Streamable :: Save (char * filename)
-{
-ofstream os (filename);
-    Write (os);
-}
+//----------------------------------------------------------------------
 
-///////////////////////////////////////////////////////////////////////////
-			      
 inline istream& operator>> (istream& is, Streamable& obj)
 {
     obj.Read (is);
     return (is);
-};
+}
 
 inline ostream& operator<< (ostream& is, Streamable& obj)
 {
     obj.Write (is);
     return (is);
-};
+}
 
-///////////////////////////////////////////////////////////////////////////
+//----------------------------------------------------------------------
 
 #endif
 
