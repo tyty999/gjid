acb7997b1035d94417f5dac0dbcc802b572f754c
msharov <msharov>
2005-09-28 15:45:07 +0000

    Implemented fb device opening/closing/mode switch

--- src/con/fb.cc
+++ src/con/fb.cc
@@ -2,12 +2,35 @@
 //
 
 #include "fbdrv.h"
+#include <linux/fb.h>
+#include <linux/kdev_t.h>
+#include <linux/major.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 namespace fbgl {
 
 CFramebuffer::CFramebuffer (void)
-: m_Modes ()
+: m_pFix (NULL),
+  m_pOrigVar (NULL),
+  m_pVar (NULL),
+  m_Device (),
+  m_Modes ()
 {
+    auto_ptr<fb_fix_screeninfo> pFix (new fb_fix_screeninfo);
+    auto_ptr<fb_var_screeninfo> pOrigVar (new fb_var_screeninfo);
+    auto_ptr<fb_var_screeninfo> pVar (new fb_var_screeninfo);
+    m_pFix = pFix.release();
+    m_pOrigVar = pOrigVar.release();
+    m_pVar = pVar.release();
+}
+
+CFramebuffer::~CFramebuffer (void)
+{
+    Close();
+    Delete (m_pFix);
+    Delete (m_pOrigVar);
+    Delete (m_pVar);
 }
 
 /*static*/ CFramebuffer& CFramebuffer::Instance (void)
@@ -18,10 +41,44 @@ CFramebuffer::CFramebuffer (void)
 
 void CFramebuffer::Open (void)
 {
+    assert (!m_Device.IsOpen());
+    string deviceName;
+    DetectDefaultDevice (deviceName);
+    m_Device.Open (deviceName);
+    m_Device.Ioctl (IOCTLID (FBIOGET_FSCREENINFO), m_pFix);
+    m_Device.Ioctl (IOCTLID (FBIOGET_VSCREENINFO), m_pOrigVar);
+    *m_pVar = *m_pOrigVar;
+    m_Screen = m_Device.Map (m_pFix->smem_len);
 }
 
 void CFramebuffer::Close (void)
 {
+    if (m_Screen.data())
+	m_Device.Unmap (m_Screen);
+    if (m_Device.IsOpen()) {
+	m_Device.Ioctl (IOCTLID (FBIOPUT_VSCREENINFO), m_pOrigVar);
+	m_Device.Close();
+    }
+}
+
+/// Detects the default framebuffer device for use with the attached tty.
+///
+/// This works by trying to open fb0 and getting the console-framebuffer
+/// mapping for the standard input device.
+///
+void CFramebuffer::DetectDefaultDevice (string& deviceName) const
+{
+    struct stat ttyStat;
+    if (fstat (STDIN_FILENO, &ttyStat))
+	throw file_exception ("fstat", "stdin");
+    const int vti = ((MAJOR(ttyStat.st_rdev) == TTY_MAJOR) ? int(MINOR(ttyStat.st_rdev)) : -1);
+    if (vti < 0)
+	throw domain_error ("this program only works on a real console, not in an xterm or ssh");
+    CFile fb;
+    fb.Open ("/dev/fb0", CFile::mode_Write);
+    struct fb_con2fbmap c2fb = { vti, 0 };
+    fb.Ioctl (IOCTLID (FBIOGET_CON2FBMAP), &c2fb);
+    deviceName.format ("/dev/fb%d", c2fb.framebuffer);
 }
 
 void CFramebuffer::LoadModes (void)
@@ -53,13 +110,18 @@ uoff_t CFramebuffer::FindClosestMode (size_t w, size_t h, size_t freq) const
     return (found);
 }
 
-void CFramebuffer::SetMode (size_t w, size_t h, size_t, size_t freq)
+void CFramebuffer::SetMode (size_t w, size_t h, size_t freq, size_t depth)
 {
+    assert (m_Device.IsOpen());
     if (m_Modes.empty())
 	LoadModes();
     uoff_t m = FindClosestMode (w, h, freq);
     if (m == m_Modes.size())
 	return;
+    CFbMode newMode (m_Modes[m]);
+    newMode.SetDepth (depth);
+    newMode.CreateVarinfo (*m_pVar);
+    m_Device.Ioctl (IOCTLID (FBIOPUT_VSCREENINFO), m_pVar);
     cout.format ("Mode closest to %ux%u@%u:\n", w, h, freq);
     cout << m_Modes[m];
 }
--- src/con/fb.h
+++ src/con/fb.h
@@ -6,22 +6,32 @@
 
 #include "fbmode.h"
 
+struct fb_fix_screeninfo;
+struct fb_var_screeninfo;
+
 namespace fbgl {
 
 class CFramebuffer {
 public:
-   static CFramebuffer&	Instance (void);
-    void		Open (void);
-    void		Close (void);
-    void		SetMode (size_t w, size_t h, size_t depth, size_t freq);
+    static CFramebuffer&	Instance (void);
+    void			Open (void);
+    void			Close (void);
+    void			SetMode (size_t w, size_t h, size_t freq, size_t depth);
 private:
     typedef vector<CFbMode>	modevec_t;
 private:
-			CFramebuffer (void);
-    void		LoadModes (void);
-    uoff_t		FindClosestMode (size_t w, size_t h, size_t freq) const;
+				CFramebuffer (void);
+    virtual		       ~CFramebuffer (void);
+    void			LoadModes (void);
+    uoff_t			FindClosestMode (size_t w, size_t h, size_t freq) const;
+    void			DetectDefaultDevice (string& deviceName) const;
 private:
-    modevec_t		m_Modes;
+    struct fb_fix_screeninfo*	m_pFix;
+    struct fb_var_screeninfo*	m_pOrigVar;
+    struct fb_var_screeninfo*	m_pVar;
+    CFile			m_Device;
+    modevec_t			m_Modes;
+    memlink			m_Screen;
 };
 
 } // namespace fbgl
