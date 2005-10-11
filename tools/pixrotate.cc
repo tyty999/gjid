c299bc1a6e372cf9d10e7d430234505062ad845a
msharov <msharov>
2005-10-11 11:06:31 +0000

    Recoded for fbgl/ustl

--- tools/pixrotate.cc
+++ tools/pixrotate.cc
@@ -1,32 +1,36 @@
-/* pixrotate.cc
-**
-**	Rotates a .pix file by 90 degrees.
-*/
+// pixrotate.cc
+//
+//	Rotates a .pix file by 90 degrees.
+//
 
 #include "../gamelib/icon.h"
 
 int main (int argc, char ** argv)
 {
-    Icon pix, rpix;
-
     if (argc < 2) {
-       cout << "Usage: " << argv[0] << " <file.pix>\n";
-       return (1);
+	cout << "Usage: " << argv[0] << " <file.pix>\n";
+	return (1);
     }
 
-    pix.Load (argv[1]);
+    memblock buf;
+    buf.read_file (argv[1]);
+    istream is (buf);
+    Icon pix;
+    is >> pix;
+
     if (pix.Height() != pix.Width()) {
-       cout << "Sorry, only square pictures are supported.\n";
-       return (0);
+	cout << "Sorry, only square pictures are supported.\n";
+	return (1);
     }
-       
-    rpix = pix;
 
-    for (WORD y = 0; y < pix.Height(); ++ y)
-       for (WORD x = 0; x < pix.Width(); ++ x)
-	  rpix.SetPixel (pix.Height() - (y + 1), x, pix.GetPixel (x, y));
+    Icon rpix (pix);
+    for (dim_t y = 0; y < pix.Height(); ++ y)
+	for (dim_t x = 0; x < pix.Width(); ++ x)
+	    rpix.SetPixel (pix.Height() - (y + 1), x, pix.GetPixel (x, y));
 
-    rpix.Save (argv[1]);
-    return (0); 
+    ostream os (buf);
+    os << rpix;
+    buf.write_file (argv[1]);
+    return (0);
 }
 
