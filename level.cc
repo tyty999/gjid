26f27da1de89b8ac95600b0aa6cf9453b974f363
msharov <msharov>
2005-10-12 00:26:31 +0000

    Removed useless m_nCrates variable

--- level.cc
+++ level.cc
@@ -27,8 +27,7 @@ size_t ObjectType::stream_size (void) const
 Level::Level (void)
 : m_Map (MAP_WIDTH * MAP_HEIGHT),
   m_Crates (),
-  m_Robot (0, 0, RobotNorthPix),
-  m_nCrates (0)
+  m_Robot (0, 0, RobotNorthPix)
 {
     fill (m_Map, FloorPix);
 }
@@ -64,16 +63,6 @@ int Level::FindCrate (coord_t x, coord_t y) const
 	if (ic->x == x && ic->y == y)
 	    return (distance (m_Crates.begin(), ic));
     return (-1);
-}
-
-void Level::SetCell (coord_t x, coord_t y, PicIndex pic)
-{
-    m_Map [y * MAP_WIDTH + x] = pic;
-}
-
-bool Level::Finished (void) const
-{
-    return (!m_nCrates && At(m_Robot.x, m_Robot.y) == ExitPix);
 }
 
 void Level::MoveRobot (RobotDir where)
@@ -126,29 +115,19 @@ void Level::MoveRobot (coord_t x, coord_t y, PicIndex pic)
     }
 }
 
-void Level::DisposeCrate (uoff_t index)
-{
-    m_Crates.erase (m_Crates.begin() + index);
-    -- m_nCrates;
-}
-
 void Level::AddCrate (coord_t x, coord_t y, PicIndex pic)
 {
     m_Crates.push_back();
     m_Crates.back().x = x;
     m_Crates.back().y = y;
     m_Crates.back().pic = pic;
-    ++ m_nCrates;
 }
 
 void Level::read (istream& is)
 {
     foreach (tilemap_t::iterator, i, m_Map)
 	is >> *i;
-    is >> m_nCrates;
-    m_Crates.resize (m_nCrates);
-    foreach (objvec_t::iterator, i, m_Crates)
-	is >> *i;
+    is >> m_Crates;
     is >> m_Robot;
 }
 
@@ -156,9 +135,7 @@ void Level::write (ostream& os) const
 {
     foreach (tilemap_t::const_iterator, i, m_Map)
 	os << *i;
-    os << m_nCrates;
-    foreach (objvec_t::const_iterator, i, m_Crates)
-	os << *i;
+    os << m_Crates;
     os << m_Robot;
 }
 
@@ -167,9 +144,7 @@ size_t Level::stream_size (void) const
     size_t s (0);
     foreach (tilemap_t::const_iterator, i, m_Map)
 	s += stream_size_of (*i);
-    s += stream_size_of (m_nCrates);
-    foreach (objvec_t::const_iterator, i, m_Crates)
-	s += stream_size_of (*i);
+    s += stream_size_of (m_Crates);
     s += stream_size_of (m_Robot);
     return (s);
 }
--- level.h
+++ level.h
@@ -73,18 +73,18 @@ class Level {
 public:
 			Level (void);
     void		Draw (CGC& gc, const picvec_t& tiles) const;
+    inline PicIndex	At (coord_t x, coord_t y) const			{ return (m_Map [y * MAP_WIDTH + x]); }
+    inline void		SetCell (coord_t x, coord_t y, PicIndex pic)	{ m_Map [y * MAP_WIDTH + x] = pic; }
+    inline bool		Finished (void) const				{ return (m_Crates.empty() && At(m_Robot.x, m_Robot.y) == ExitPix); }
+    inline void		DisposeCrate (uoff_t index)			{ m_Crates.erase (m_Crates.begin() + index); }
+    void		AddCrate (coord_t x, coord_t y, PicIndex pic);
     void		MoveRobot (RobotDir where);
     void		MoveRobot (coord_t x, coord_t y, PicIndex pic);
-    void		DisposeCrate (uoff_t index);
-    void		AddCrate (coord_t x, coord_t y, PicIndex pic);
-    void		SetCell (coord_t x, coord_t y, PicIndex pic);
     void		read (istream& is);
     void		write (ostream& os) const;
     size_t		stream_size (void) const;
-    bool		Finished (void) const;
     int			FindCrate (coord_t x, coord_t y) const;
     bool		CanMoveTo (coord_t x, coord_t y, RobotDir where) const;
-    inline PicIndex	At (coord_t x, coord_t y) const	{ return (m_Map [y * MAP_WIDTH + x]); }
 private:
     typedef vector<PicIndex>	tilemap_t;
     typedef vector<ObjectType>	objvec_t;
@@ -92,7 +92,6 @@ private:
     tilemap_t		m_Map;
     objvec_t		m_Crates;
     ObjectType		m_Robot;
-    size_t		m_nCrates;
 };
 
 STD_STREAMABLE (ObjectType)
