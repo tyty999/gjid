59676d0b38ae736bed3c44cdb824f1b9de57a292
Mike Sharov <msharov@users.sourceforge.net>
2011-07-25 14:08:44 -0400

    Merge startup.cc into app.cc

--- app.cc
+++ app.cc
@@ -3,6 +3,8 @@
 
 #include "app.h"
 #include "fb.h"
+#include <signal.h>
+#include <stdio.h>
 
 //----------------------------------------------------------------------
 
@@ -82,3 +84,31 @@ void CApplication::OnKey (key_t key)
 }
 
 void CApplication::OnQuit (void) { SetFlag (f_QuitRequested); }
+
+//----------------------------------------------------------------------
+
+/// Called when a signal is received.
+static void OnSignal (int sig)
+{
+    printf ("[S] Fatal error: %s\n", strsignal(sig));
+    exit (-sig);
+}
+
+/// Called by the framework on unrecoverable exception handling errors.
+static void Terminate (void)
+{
+    printf ("[T] Unexpected fatal error\n");
+    exit (EXIT_FAILURE);
+}
+
+/// Installs OnSignal as handler for signals.
+extern "C" void InstallCleanupHandlers (void)
+{
+    static const int8_t c_Signals[] = {
+	SIGILL, SIGABRT, SIGBUS,  SIGFPE,  SIGSEGV, SIGSYS, SIGALRM, SIGXCPU,
+	SIGHUP, SIGINT,  SIGQUIT, SIGTERM, SIGCHLD, SIGXFSZ, SIGPWR, SIGPIPE
+    };
+    for (uoff_t i = 0; i < VectorSize(c_Signals); ++ i)
+	signal (c_Signals[i], OnSignal);
+    std::set_terminate (Terminate);
+}
